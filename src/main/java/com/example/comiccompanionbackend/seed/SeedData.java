b46b06f58a7be9d38328bfa348cb17c700c5e72b
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-19 18:02:44 -0400

    Create SeedData class

---
+++ src/main/java/com/example/comiccompanionbackend/seed/SeedData.java
@@ -0,0 +1,10 @@
+package com.example.comiccompanionbackend.seed;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class SeedData implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
