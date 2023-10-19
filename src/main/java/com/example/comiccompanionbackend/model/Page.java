f25b0818b1a08f9bafdab28faf323bd56687a8d7
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-19 17:24:56 -0400

    Add variables and annotations to page model

--- src/main/java/com/example/comiccompanionbackend/model/Page.java
+++ src/main/java/com/example/comiccompanionbackend/model/Page.java
@@ -1,4 +1,19 @@
 package com.example.comiccompanionbackend.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "pages")
 public class Page {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
 }
