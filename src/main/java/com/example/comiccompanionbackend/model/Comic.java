2037cf4ace311d43dc59c712c05124f05eef0ff7
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-19 17:29:24 -0400

    Generate getters setters to Comic model

--- src/main/java/com/example/comiccompanionbackend/model/Comic.java
+++ src/main/java/com/example/comiccompanionbackend/model/Comic.java
@@ -15,4 +15,34 @@ public class Comic {
 
     @Column
     private String description;
+
+    public Comic(Long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
