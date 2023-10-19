4f074337643abaf283fcf7db15bd8efdda4399d0
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-19 17:10:46 -0400

    first commit

---
+++ src/main/java/com/example/comiccompanionbackend/ComicCompanionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.comiccompanionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicCompanionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicCompanionBackendApplication.class, args);
+    }
+
+}
