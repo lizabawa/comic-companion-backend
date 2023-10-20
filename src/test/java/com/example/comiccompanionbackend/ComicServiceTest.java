a48a5d3c796d41769a9211d2f496f72bad499c8b
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-20 12:53:51 -0400

    Add mock data to ComicServiceTest

--- src/test/java/com/example/comiccompanionbackend/ComicServiceTest.java
+++ src/test/java/com/example/comiccompanionbackend/ComicServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.comiccompanionbackend;
 
 import com.example.comiccompanionbackend.controller.ComicController;
+import com.example.comiccompanionbackend.model.Comic;
+import com.example.comiccompanionbackend.model.Page;
 import com.example.comiccompanionbackend.service.ComicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,4 +17,13 @@ public class ComicServiceTest {
 
     @MockBean
     private ComicController comicController;
+
+    Comic comic1 = new Comic(1L, "Comic 1", "Comic 1 Description");
+    Comic comic2 = new Comic(2L, "Comic 2", "Comic 2 Description");
+    Comic comic3 = new Comic(3L, "Comic 3", "Comic 3 Description");
+
+    Page c1pg1 = new Page(1L, "mech-bros-images/Noxcomic_pg01_02.png", 1);
+    Page c1pg2 = new Page(2L, "mech-bros-images/Noxcomic_pg02_02.png", 2);
+    Page c1pg3 = new Page(3L, "mech-bros-images/Noxcomic_pg03_05.png", 3);
+
 }
