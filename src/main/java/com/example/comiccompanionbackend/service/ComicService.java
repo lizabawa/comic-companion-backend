003a1e11239eaf9077ef58ae07c8ca25fb5d3767
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-20 12:29:01 -0400

    Add constructor to ComicService

--- src/main/java/com/example/comiccompanionbackend/service/ComicService.java
+++ src/main/java/com/example/comiccompanionbackend/service/ComicService.java
@@ -1,7 +1,22 @@
 package com.example.comiccompanionbackend.service;
 
+import com.example.comiccompanionbackend.repository.ComicRepository;
+import com.example.comiccompanionbackend.repository.PageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ComicService {
+    private ComicRepository comicRepository;
+    private PageRepository pageRepository;
+
+    @Autowired
+    public void setComicRepository(ComicRepository comicRepository) {
+        this.comicRepository = comicRepository;
+    }
+
+    @Autowired
+    public ComicService(PageRepository pageRepository) {
+        this.pageRepository = pageRepository;
+    }
 }
