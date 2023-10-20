a61a8c778388e52de6ab75654d4759e873d7189f
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-20 10:31:22 -0400

    Add pages for red ranger comic to seed data

--- src/main/java/com/example/comiccompanionbackend/seed/SeedData.java
+++ src/main/java/com/example/comiccompanionbackend/seed/SeedData.java
@@ -37,6 +37,35 @@ public class SeedData implements CommandLineRunner {
         rrpg1.setPageNumber(1);
         rrpg1.setUrl("red-ranger-images/RedRanger_comic_001.png");
 
+        Page rrpg2 = new Page();
+        rrpg1.setPageNumber(2);
+        rrpg1.setUrl("red-ranger-images/RedRanger_comic_002.png");
+
+        Page rrpg3 = new Page();
+        rrpg1.setPageNumber(3);
+        rrpg1.setUrl("red-ranger-images/RedRanger_comic_003.png");
+
+        Page rrpg4 = new Page();
+        rrpg1.setPageNumber(4);
+        rrpg1.setUrl("red-ranger-images/RedRanger_comic_004.png");
+
+        Page rrpg5 = new Page();
+        rrpg1.setPageNumber(5);
+        rrpg1.setUrl("red-ranger-images/RedRanger_comic_005.png");
+
+        Page rrpg6 = new Page();
+        rrpg1.setPageNumber(6);
+        rrpg1.setUrl("red-ranger-images/RedRanger_comic_006.png");
+
+        Page rrpg7 = new Page();
+        rrpg1.setPageNumber(7);
+        rrpg1.setUrl("red-ranger-images/RedRanger_comic_007.png");
+
+        Page rrpg8 = new Page();
+        rrpg1.setPageNumber(8);
+        rrpg1.setUrl("red-ranger-images/RedRanger_comic_008.png");
+
+        //TWO SISTERS
         Comic twoSisters = new Comic();
         twoSisters.setTitle("The Tale of Two Sisters");
         twoSisters.setDescription("MechBros description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, mollitia. Neque quibusdam impedit corrupti perferendis! Voluptatum accusamus rerum adipisci ea excepturi inventore, vel magni porro blanditiis consectetur debitis fugit molestias!");
