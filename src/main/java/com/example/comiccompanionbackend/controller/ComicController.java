d422df2ea132b4c879fae524fe62094964ec3feb
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-20 12:31:41 -0400

    Add constructor to ComicController

--- src/main/java/com/example/comiccompanionbackend/controller/ComicController.java
+++ src/main/java/com/example/comiccompanionbackend/controller/ComicController.java
@@ -1,9 +1,21 @@
 package com.example.comiccompanionbackend.controller;
 
+import com.example.comiccompanionbackend.service.ComicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api/v1/comics")
 public class ComicController {
+    Logger log = Logger.getLogger(ComicController.class.getName());
+
+    private ComicService comicService;
+
+    @Autowired
+    public void setComicService(ComicService comicService) {
+        this.comicService = comicService;
+    }
 }
--- src/main/java/com/example/comiccompanionbackend/service/ComicService.java
+++ src/main/java/com/example/comiccompanionbackend/service/ComicService.java
@@ -5,10 +5,14 @@ import com.example.comiccompanionbackend.repository.PageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.logging.Logger;
+
 @Service
 public class ComicService {
     private ComicRepository comicRepository;
     private PageRepository pageRepository;
+
+    Logger log = Logger.getLogger(ComicService.class.getName());
 
     @Autowired
     public void setComicRepository(ComicRepository comicRepository) {
