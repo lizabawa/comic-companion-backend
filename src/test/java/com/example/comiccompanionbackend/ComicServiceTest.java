56d8e2f24cf8d9fb5a05b830549958a3903777e3
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-20 12:49:38 -0400

    Add variables and annotations to ComicServiceTest

--- src/test/java/com/example/comiccompanionbackend/ComicServiceTest.java
+++ src/test/java/com/example/comiccompanionbackend/ComicServiceTest.java
@@ -1,4 +1,18 @@
 package com.example.comiccompanionbackend;
 
+import com.example.comiccompanionbackend.controller.ComicController;
+import com.example.comiccompanionbackend.service.ComicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ComicService.class)
 public class ComicServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ComicController comicController;
 }
