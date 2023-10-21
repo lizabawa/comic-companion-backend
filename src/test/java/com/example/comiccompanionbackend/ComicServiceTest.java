c57a45e174b68797f376607819b4d9e55879523a
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-21 14:42:05 -0400

    Clean up ComicServiceTest

--- src/test/java/com/example/comiccompanionbackend/ComicServiceTest.java
+++ src/test/java/com/example/comiccompanionbackend/ComicServiceTest.java
@@ -1,16 +1,12 @@
 package com.example.comiccompanionbackend;
 
-import com.example.comiccompanionbackend.controller.ComicController;
 import com.example.comiccompanionbackend.model.Comic;
 import com.example.comiccompanionbackend.model.Page;
 import com.example.comiccompanionbackend.repository.ComicRepository;
 import com.example.comiccompanionbackend.repository.PageRepository;
 import com.example.comiccompanionbackend.service.ComicService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -28,30 +24,10 @@ public class ComicServiceTest {
     private ComicService comicService;
 
     @Mock
-    private ComicController comicController;
-
-    @Mock
     private ComicRepository comicRepository;
 
     @Mock
     private PageRepository pageRepository;
-
-//    @BeforeEach
-//    public void createComic(){
-//        Comic comic1 = new Comic(1L, "Comic 1", "Comic 1 Description");
-//        Comic comic2 = new Comic(2L, "Comic 2", "Comic 2 Description");
-//        Comic comic3 = new Comic(3L, "Comic 3", "Comic 3 Description");
-//        comicRepository.save(comic1);
-//        comicRepository.save(comic2);
-//        comicRepository.save(comic3);
-//
-//        Page c1pg1 = new Page(1L, "mech-bros-images/Noxcomic_pg01_02.png", 1);
-//        Page c1pg2 = new Page(2L, "mech-bros-images/Noxcomic_pg02_02.png", 2);
-//        Page c1pg3 = new Page(3L, "mech-bros-images/Noxcomic_pg03_05.png", 3);
-//        pageRepository.save(c1pg1);
-//        pageRepository.save(c1pg2);
-//        pageRepository.save(c1pg3);
-//    }
 
     @Test
     public void testGetHelloWorld() {
