29a45d6aec39ef3cffd260fa38c02a147f17c6a3
Elizabeth Yang <elizabethyang928@gmail.com>
2023-10-22 21:41:19 -0400

    Move CorsConfig class

--- src/main/java/com/example/comiccompanionbackend/CorsConfig.java
+++ src/main/java/com/example/comiccompanionbackend/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.example.comiccompanionbackend;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
